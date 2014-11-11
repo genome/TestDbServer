1079150e7aca220510528ee2efc10111520f44c2
Nathaniel Nutter <iam@nnutter.com>
2014-11-11 16:43:23 -0600

    add real_owner to database

--- lib/TestDbServer/Schema/Result/Database.pm
+++ lib/TestDbServer/Schema/Result/Database.pm
@@ -6,4 +6,12 @@ __PACKAGE__->add_columns(qw(database_id host port name owner create_time expire_
 __PACKAGE__->set_primary_key('database_id');
 __PACKAGE__->belongs_to(template => 'TestDbServer::Schema::Result::Template', 'template_id');
 
+sub real_owner {
+    my $self = shift;
+    my $dbh = $self->result_source->storage->dbh();
+    my $statement = "SELECT rolname FROM pg_database JOIN pg_authid ON pg_database.datdba = pg_authid.oid WHERE pg_database.datname = ?;";
+    my $row = $dbh->selectrow_arrayref($statement, undef, $self->name);
+    return $row->[0];
+}
+
 1;
